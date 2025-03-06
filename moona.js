d65203081324f9c078c05ba55cf423b55085fb89
Matt Born <mattborn@users.noreply.github.com>
2025-03-06 13:59:24 -0700

    New hero

--- moona.js
+++ moona.js
@@ -1,3 +1,5 @@
+const q = document.querySelectorAll.bind(document)
+
 /* tracking */
 ;((c, l, a, r, i, t, y) => {
   c[a] =
@@ -11,3 +13,6 @@
   y = l.getElementsByTagName(r)[0]
   y.parentNode.insertBefore(t, y)
 })(window, document, 'clarity', 'script', 'o8cjnil62g')
+
+/* apply image background */
+q('[data-bg]').forEach(el => (el.style.backgroundImage = `url('${el.dataset.bg}')`))
